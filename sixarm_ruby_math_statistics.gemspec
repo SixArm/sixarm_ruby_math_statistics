268de16a64e987434e5263af6a2d352d0e85a216
SixArm <sixarm@sixarm.com>
2010-10-17 19:16:49 -0700

    first

---
+++ sixarm_ruby_math_statistics.gemspec
@@ -0,0 +1,18 @@
+Gem::Specification.new do |s|
+
+  s.name              = "sixarm_ruby_math_statistics"
+  s.summary           = "SixArm Ruby Gem: Math statitics methods for sum, mean, median, mode, variance, deviation, etc."
+  s.version           = "1.1.8"
+  s.author            = "SixArm"
+  s.email             = "sixarm@sixarm.com"
+  s.homepage          = "http://sixarm.com/"
+  s.signing_key       = '/home/sixarm/keys/certs/sixarm.com.rsa.private.key.and.certificate.pem'
+  s.cert_chain        = ['/home/sixarm/keys/certs/sixarm.pem']
+
+  s.platform          = Gem::Platform::RUBY
+  s.require_path      = 'lib'
+  s.has_rdoc          = true
+  s.files             = ['README.rdoc','LICENSE.txt','lib/sixarm_ruby_math_statistics.rb']
+  s.test_files        = ['test/sixarm_ruby_math_statistics_test.rb']
+
+end
---
+++ test/sixarm_ruby_math_statistics_test.rb
@@ -0,0 +1,48 @@
+require 'test/unit'
+require 'sixarm_ruby_math_statistics'
+
+class MathStatiticsTest < Test::Unit::TestCase
+
+ def test_sum
+  assert_equal(0,[].sum)
+  assert_equal(1,[1].sum)
+  assert_equal(6,[1,2,3].sum)
+ end
+
+ def test_mean
+  assert_equal(nil,[].mean)
+  assert_equal(1.0,[1].mean)
+  assert_equal(1.5,[1,2].mean)
+  assert_equal(4.0,[1,2,9].mean)
+ end
+
+ def test_median
+  assert_equal(nil,[].median)
+  assert_equal(1.0,[1].median)
+  assert_equal(1.5,[1,2].median)
+  assert_equal(2.0,[1,2,9].median)
+ end
+
+ def test_sum_of_squares
+  assert_equal(0,[].sum_of_squares)
+  assert_equal(1.0,[1].sum_of_squares)
+  assert_equal(5.0,[1,2].sum_of_squares)
+  assert_equal(14.0,[1,2,3].sum_of_squares)
+ end
+
+ def test_variance
+  assert_equal(nil,[].variance)
+  assert_equal(0.0,[1].variance)
+  assert_equal(0.25,[1,2].variance)
+  assert_equal(1.25,[1,2,3,4].variance)
+ end
+
+ def test_deviation
+  assert_equal(nil,[].deviation)
+  assert_equal(0.0,[1].deviation)
+  assert_equal(0.5,[1,2].deviation)
+  assert_in_delta(0.8,[2,2,4,2,2].deviation,0.001)
+ end
+
+end
+
